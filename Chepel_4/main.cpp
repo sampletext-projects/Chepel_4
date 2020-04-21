85c96803ed5081b7b72408523b42c54416d5d650
sampletext32 <sampletext32@bk.ru>
2020-04-21 21:06:54 +0300

    Add project

---
+++ Chepel_4/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const double real_pi = 3.14159265;
+
+double Pi(double eps)
+{
+	double pi = 1;
+	double k = 1;
+	while(abs(real_pi - pi * 2) >= eps)
+	{
+		pi *= ((k + 1) * (k + 1)) / (k * (k + 2));
+		k += 2;
+	}
+	return pi * 2;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Автор: Чепель Егор Максимович\n";
+	cout << "Программа расчёт числа PI" << endl;
+
+	cout << Pi(0.01) << endl;
+	cout << Pi(0.001) << endl;
+	cout << Pi(0.0001) << endl;
+
+	system("pause");
+
+	return 0;
+}
